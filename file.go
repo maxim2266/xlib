43d525d3ed654f9ef17dca8ea7a6ba7391e436d9
Maxim <maxim2266@outlook.com>
2018-11-12 12:23:53 +0000

    Package header added.

--- file.go
+++ file.go
@@ -26,6 +26,7 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+// Package xlib is an ever growing collection of useful Go functions.
 package xlib
 
 import (
