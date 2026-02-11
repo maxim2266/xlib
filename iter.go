d3c049581c4468d8446412ee3ffb5537c94e5439
Maxim <maxim2266@outlook.com>
2026-02-11 22:18:21 +0000

    Iterator functions added.

---
+++ iter.go
@@ -0,0 +1,67 @@
+package xlib
+
+import "iter"
+
+// MapIt creates a new iterator that converts each item from the original
+// iterator via the given function.
+func MapIt[T, U any](src iter.Seq[T], fn func(T) U) iter.Seq[U] {
+	return func(yield func(U) bool) {
+		for v := range src {
+			if !yield(fn(v)) {
+				break
+			}
+		}
+	}
+}
+
+// FilterIt creates a new iterator that filters items from the original
+// iterator using the supplied predicate.
+func FilterIt[T any](src iter.Seq[T], pred func(T) bool) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for v := range src {
+			if pred(v) && !yield(v) {
+				break
+			}
+		}
+	}
+}
+
+// PipeIt creates a new iterator that runs the original iterator in
+// a dedicated goroutine.
+func PipeIt[T any](src iter.Seq[T]) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		// channels
+		pipe, done := make(chan T, 10), make(chan struct{})
+
+		defer close(done)
+
+		// feeder
+		go func() {
+			defer func() {
+				// we don't want to close the channel on panic, because
+				// that would allow the reader loop to exit while the panic
+				// is still in progress
+				if p := recover(); p != nil {
+					panic(p)
+				}
+
+				close(pipe)
+			}()
+
+			for v := range src {
+				select {
+				case pipe <- v:
+				case <-done:
+					return
+				}
+			}
+		}()
+
+		// reader loop
+		for v := range pipe {
+			if !yield(v) {
+				break
+			}
+		}
+	}
+}
---
+++ iter_test.go
@@ -0,0 +1,43 @@
+package xlib
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestMapIt(t *testing.T) {
+	src := []int{1, 2, 3, 4}
+	res := slices.Collect(MapIt(slices.Values(src), func(x int) int { return 2 * x }))
+
+	if !slices.Equal(res, []int{2, 4, 6, 8}) {
+		t.Errorf("invalid result: %v", res)
+		return
+	}
+}
+
+func TestFilterIt(t *testing.T) {
+	src := []int{1, 2, 3, 4}
+	res := slices.Collect(FilterIt(slices.Values(src), func(x int) bool { return x&1 != 0 }))
+
+	if !slices.Equal(res, []int{1, 3}) {
+		t.Errorf("invalid result: %v", res)
+		return
+	}
+}
+
+func TestPipeIt(t *testing.T) {
+	const N = 1000
+
+	src := make([]int, N)
+
+	for i := range N {
+		src[i] = i
+	}
+
+	res := slices.Collect(PipeIt(slices.Values(src)))
+
+	if !slices.Equal(res, src) {
+		t.Error("arrays don't match")
+		return
+	}
+}
